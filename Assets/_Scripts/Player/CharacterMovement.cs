3c5bb6144acd649aae025ac0e5ca7be37bae5299
Pedro Bianchi <pedro.bianchi0208@gmail.com>
2026-03-26 14:48:21 -0300

    Add tilemap para a scene de teste do projeot e Add 3 scripts para a movimentacao do player usando a regra de SRP

---
+++ Assets/_Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SunnySword.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 1;
+
+        private new Rigidbody2D rigidbody;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void Move(float horizontalInput, float verticalInput)
+        {
+            Vector2 direction = new(horizontalInput, verticalInput);
+            rigidbody.linearVelocity = direction * speed;
+        }
+
+        public void Stop()
+        {
+            rigidbody.linearVelocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace SunnySword.Player
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public float HorizontalInput { get; private set; }
+        public float VerticalInput { get; private set; }
+        public bool AttackInput { get; private set; }
+        public bool InteractionInput { get; private set; }
+
+        private void Update()
+        {
+            HorizontalInput = Input.GetAxis("Horizontal");
+            VerticalInput = Input.GetAxis("Vertical");
+
+            if (Input.GetButtonDown("Fire1")) AttackInput = true;
+            if (Input.GetButtonDown("Fire2")) InteractionInput = true;
+        }
+    }
+}
